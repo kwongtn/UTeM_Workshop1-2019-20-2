b8a7f1a0b41162b12faaed5cd85005176b1cdc3d
KwongTN <tungnan5636@gmail.com>
2020-06-11 20:57:28 +0800

    Shifted parameters to separate file

---
+++ src/client/parameters.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+
+const std::string DB_ADDRESS = "127.0.0.1";
+const int DB_PORT = 8689;
+const std::string DB_LOGIN = "root";
+const std::string DB_PASS = "password";
+const std::string SCHEMA = "ATT_MGMT";
