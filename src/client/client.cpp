3d11090dbe17050eefb26dbd25ec94ab7746281b
KwongTN <tungnan5636@gmail.com>
2020-06-28 23:27:40 +0800

    Enabled back login

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -155,8 +155,8 @@ int main() {
 	keybd_event(VK_RETURN, 0x1c, 0, 0);
 	keybd_event(VK_RETURN, 0x1c, KEYEVENTF_KEYUP, 0);
 	keybd_event(VK_MENU, 0x38, KEYEVENTF_KEYUP, 0);
-	while (!login()) {}
 	*/
+	while (!login()) {}
 Relogin:
 
 	unsigned int selection = 0;
